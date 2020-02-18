4dde8469723cea5f476e7f4e64f00292558f696a
Kunal Katarya <kunal.rox@gmail.com>
2020-02-18 18:35:50 +0000

    model learns now

--- part2_claim_classifier.py
+++ part2_claim_classifier.py
@@ -7,9 +7,12 @@ from torch.autograd import Variable
 from torch.utils.data import DataLoader, Dataset
 import sys
 
-def to_categorical(y, num_classes):
-    """1-hot encodes a tensor"""
-    return np.eye(num_classes, dtype='uint8')[y]
+
+def get_accuracy(y_out, y_target):
+  y_pred = y_out >= 0.5       # a Tensor of 0s and 1s
+  num_correct = torch.sum(y_target==y_pred.float())  # a Tensor
+  acc = (num_correct.item() * 100.0 / len(y_target))  # scalar
+  return acc
 
 
 class PrepareData(Dataset):
@@ -27,7 +30,6 @@ class PrepareData(Dataset):
 
 
 class ClaimClassifier():
-
     def __init__(self):
         """
         Feel free to alter this as you wish, adding instance variables as
@@ -36,11 +38,9 @@ class ClaimClassifier():
         super(ClaimClassifier, self).__init__()
         self.model = nn.Sequential(
             nn.Linear(9,5),
-            nn.Tanh(),
-            nn.Linear(5,5),
             nn.ReLU(),
             nn.Linear(5,1),
-            #nn.Sigmoid(),
+            nn.Sigmoid(),
         )
 
     def _preprocessor(self, X_raw):
@@ -97,7 +97,7 @@ class ClaimClassifier():
         ds = DataLoader(ds, batch_size=50, shuffle=True)
 
         n_epochs = 30
-        optimizer = torch.optim.Adam(self.model.parameters(), lr=0.1)
+        optimizer = torch.optim.Adam(self.model.parameters(), lr=0.01)
         cost_func = nn.BCELoss()
 
         losses = []
@@ -110,11 +110,10 @@ class ClaimClassifier():
                 _y = Variable(_y).float()
 
                 #========forward pass=====================================
-                prediction = self.model(_x).float()
-                #print(prediction)
-                yhat = torch.tensor((prediction >= 0.5), dtype=torch.float32, requires_grad=True)
+                yhat = self.model(_x).float()
                 loss = cost_func(yhat, _y)
-                acc = torch.eq(yhat.round(), _y).float().mean() # accuracy
+                accuracy = get_accuracy(yhat, _y)
+                #acc = torch.eq(yhat.round(), _y).float().mean() # accuracy
 
                 #=======backward pass=====================================
                 self.model.zero_grad() # zero the gradients on each pass before the update
@@ -124,13 +123,14 @@ class ClaimClassifier():
                 #print(loss.data)
 
                 losses.append(loss.data)
-                accuracies.append(acc.data)
+                accuracies.append(accuracy)
 
                 '''for n,x in self.model.named_modules():
                     if isinstance(x, nn.Linear):
                         print(x.__dict__.keys())
 
                 print(optimizer.__dict__.keys())'''
+                #breakpoint()
 
             if epoch % 1 == 0:
                 print("[{}/{}], loss: {} acc: {}".format(epoch,
