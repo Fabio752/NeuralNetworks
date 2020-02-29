0fb1261430adfd0877cdbc166c1340c1ddd6004d
Lin Lee <lhl2617@corona49.doc.ic.ac.uk>
2020-02-29 20:05:34 +0000

    retrain

--- part2_claim_classifier.py
+++ part2_claim_classifier.py
@@ -326,10 +326,10 @@ class ClaimClassifier():
             X_raw = X_raw.values
 
         X_clean = self._preprocessor(X_raw)
-        if debug:
-            print("Xclean data type: ", type(X_clean))
-            print("Data type of the return of preprocessor", type(self._preprocessor(X_raw)))
-            print("self.val type: ", type(self.val))
+        # if debug:
+        #     print("Xclean data type: ", type(X_clean))
+        #     print("Data type of the return of preprocessor", type(self._preprocessor(X_raw)))
+        #     print("self.val type: ", type(self.val))
         temp1 = torch.from_numpy(X_clean)
         temp2 = Variable(temp1)
         X_clean = temp2.float()
